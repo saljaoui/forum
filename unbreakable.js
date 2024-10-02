b49c70b0911d9101ead2afac4a13d8e677474c35
saljaoui <soufian2022ff@gmail.com>
2024-10-02 23:57:04 +0100

    update

---
+++ unbreakable.js
@@ -0,0 +1,12 @@
+function split(str, sep = ",") {
+let res = [];
+if (sep === "") {
+    for (let i = 0; i < str.length ; i++) {
+        res.push(str[i])
+    }
+    return res
+}
+for (let i = 0;  i < str.length; i++)
+    
+}
+split("soufian", "")
