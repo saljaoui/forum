da2ee68a8137349f0f61f520132a44361fa510f4
mbakhouc <bakhouch2019@gmail.com>
2024-10-17 18:02:54 +0100

    deleting gorilla

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module forum-project
 go 1.22.3
 
 require (
-	github.com/gorilla/mux v1.8.1 
 	github.com/mattn/go-sqlite3 v1.14.24
 	golang.org/x/crypto v0.28.0
 )
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"forum-project/internal/database"
 	"forum-project/internal/handlers"
 
-	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -16,23 +15,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	r := mux.NewRouter()
 
 	// API routes
-	r.HandleFunc("/api/register", handlers.RegisterHandler).Methods("POST")
-	r.HandleFunc("/api/login", handlers.LoginHandler).Methods("POST")
+	http.HandleFunc("/api/register", handlers.RegisterHandler)
+	http.HandleFunc("/api/login", handlers.LoginHandler)
 
 	// Serve static files
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	
 
 	// Serve HTML templates
-	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/login.html")
 	})
-	r.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "templates/register.html")
 	})
 
 	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
