57ae74907631c8b7ac16a6ff197637fa991f025c
saljaoui <soufian2022ff@gmail.com>
2024-11-05 18:48:24 +0100

    test to conction backend and frontend

--- backend/internal/handlers/handlers.go
+++ backend/internal/handlers/handlers.go
@@ -1,12 +1,36 @@
 package handlers
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func TestHandlers(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	response := Response{
+		Message: "Hello, World!",
+		Status:  "success",
+	}
+
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(200)
+	w.Write(jsonData)
 }
