dafbbf28e6664073d2bdf31e202831e37d1ea28c
saljaoui <soufian2022ff@gmail.com>
2024-11-07 11:37:47 +0100

    test to conction backend and frontend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,16 +1,25 @@
-package main 
+package main
 
 import (
-    "fmt"
-    "net/http"
-    "forum-project/backend/internal/handlers"
+	"fmt"
+	"log"
+	"net/http"
+
+	"forum-project/backend/internal/handlers"
 )
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", handlers.TestHandlers)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.TestHandlers)
+    mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../../frontend/static"))))    
+	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../../frontend/templates/login.html")
+	})
 
-    err := http.ListenAndServe(":3333", mux)
-    fmt.Println(err)
+    fmt.Println("Server running at :3333")
+    fmt.Println("http://localhost:3333/")
+	err := http.ListenAndServe(":3333", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
+	}
 }
-
