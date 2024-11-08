bb9ca7cbb70af21d018666ceb7a2576dafbe710a
saljaoui <soufian2022ff@gmail.com>
2024-11-08 16:26:41 +0100

    login page Update

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -7,21 +7,22 @@ import (
 )
 
 func InitDB() error {
-	if _, err := os.Stat("../../app.db"); os.IsNotExist(err) {
+	if _, err := os.Stat("../internal/database/app.db"); os.IsNotExist(err) {
 		fmt.Println("Creating new database file...")
-		db, err := sql.Open("sqlite3", "../../app.db")
+		db, err := sql.Open("sqlite3", "../internal/database/app.db")
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to open database: %w", err)
 		}
 		defer db.Close()
-		sqlFile, err := os.ReadFile("../internal/database/database.sql")
+		sqlFile, err := os.ReadFile("../internal/database/init-db.sql")
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to read SQL file: %w", err)
 		}
 		_, err = db.Exec(string(sqlFile))
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to execute SQL file: %w", err)
 		}
+		fmt.Println("Database initialized successfully.")
 	}
 	return nil
 }
