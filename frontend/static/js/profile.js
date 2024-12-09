260bfa21e2e42aaf76eaf7cb0c22265e03fb511f
omrharbi <rharbuomar@gmail.com>
2024-12-09 18:10:21 +0100

    done

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -17,7 +17,7 @@ profileNav.forEach((navItem) => {
 
 import { likes } from "./likes.js";
 export default async function fetchData() {
-  const responce = await fetch("http://localhost:3333/api/profile/posts", {
+  const responce = await fetch("/api/profile/posts", {
     method: "GET",
   });
   if (responce.ok) {
@@ -38,21 +38,21 @@ export default async function fetchData() {
           <img src="../static/imgs/profilePic.png" class="avatar" alt="Profile picture" />
           <div class="user-info">
             <div class="display-name">${
-              ele.FirstName + " " + ele.LastName
+              ele.firstName + " " + ele.lastName
             }</div>
             <span class="username">@aoc.bsky.social</span>
             <span class="timestamp">· ${date.getHours()}h</span>
           </div>
         </div>
         <div class="post-content">
-            ${ele.Content}
+            ${ele.content}
         </div>
         <div class="post-actions">
-          <div class="action active" id="likes" data-id_card="${ele.Card_Id}" >
+          <div class="action active" id="likes" data-id_card="${ele.id}" >
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M10 19c-.072 0-.145 0-.218-.006A4.1 4.1 0 0 1 6 14.816V11H2.862a1.751 1.751 0 0 1-1.234-2.993L9.41.28a.836.836 0 0 1 1.18 0l7.782 7.727A1.751 1.751 0 0 1 17.139 11H14v3.882a4.134 4.134 0 0 1-.854 2.592A3.99 3.99 0 0 1 10 19Zm0-17.193L2.685 9.071a.251.251 0 0 0 .177.429H7.5v5.316A2.63 2.63 0 0 0 9.864 17.5a2.441 2.441 0 0 0 1.856-.682A2.478 2.478 0 0 0 12.5 15V9.5h4.639a.25.25 0 0 0 .176-.429L10 1.807Z"></path>
                 </svg>
-            <span id="is_liked">${ele.Likes}</span>
+            <span id="is_liked">${ele.likes}</span>
           </div>
            <div class="action" id="dilike">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
